511f339917bb99f5a070e1f4d4db8096d08c8a69
ge34jit <ge34jit@mytum.de>
2022-02-20 17:14:29 +0100

    js classes

---
+++ classes.js
@@ -0,0 +1,12 @@
+class Person {
+    constructor() {
+        this.name = "Bakar";
+    }
+
+    printMyName() {
+        console.log(this.name);
+    }
+}
+
+const guy = new Person();
+guy.printMyName();
