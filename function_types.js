d9b896f0e43e75ffcefb612241387223ee5171c3
ge34jit <ge34jit@mytum.de>
2022-02-20 16:12:12 +0100

    function declaration types

---
+++ function_types.js
@@ -0,0 +1,16 @@
+// classic
+function func(x) {
+    return x*x;
+}
+
+// arrow
+const f1 = x => x*x;
+
+const f2 = (x, y) => {
+    return x+y;
+}
+
+// 
+const ff = function() {
+    return "hello world";
+};
